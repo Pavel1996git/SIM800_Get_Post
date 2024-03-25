d3ad53433b6be4af0f7728d6c75e307fbd611699
Pavel <arruin1996@gmail.com>
2024-03-25 14:39:35 +0100

    vv0

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
+  console.log('Received HTTP request:');
+  console.log('Method:', req.method);
+  console.log('URL:', req.url);
+  console.log('Headers:', req.headers);
+  
   if (req.method === 'POST' && req.url === '/data') {
     const chunks = [];
+    let totalBytesReceived = 0; // Переменная для отслеживания общего размера принятых байт
 
     req.on('data', (chunk) => {
       // Собираем все байты
       chunks.push(chunk);
+      totalBytesReceived += chunk.length; // Увеличиваем счетчик на размер полученного куска данных
     });
 
     req.on('end', () => {
@@ -21,7 +28,6 @@ const server = http.createServer((req, res) => {
           res.statusCode = 500;
           res.end('Internal Server Error');
         } else {
-          console.log('Данные успешно записаны в файл data.bin');
           res.statusCode = 200;
           res.end('Данные успешно записаны');
         }
@@ -34,25 +40,26 @@ const server = http.createServer((req, res) => {
       console.log('Десятичные значения байтов:');
       byteArray.forEach((byte) => {
         console.log(byte);
+      });
 
-        // Запись десятичных значений байтов в файл data.txt через пробел
-        fs.appendFile('data.txt', byte.toString() + ' ', (err) => {
-          if (err) {
-            console.error('Ошибка записи в файл data.txt:', err);
-          }
-        });
+      // Вывод размера принятых байт в консоль
+      console.log('Размер принятых байт:', totalBytesReceived);
+
+      // Запись десятичных значений байтов в файл data.txt
+      fs.appendFile('data.txt', byteArray.join(','), (err) => {
+        if (err) {
+          console.error('Ошибка записи в файл data.txt:', err);
+        }
       });
     });
   } else if (req.method === 'GET' && req.url === '/getYes') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('GetVan');
-    console.log('GetVan');
   } else if (req.method === 'GET') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('GetZer');
-    console.log('GetZer');
   } else {
     res.statusCode = 404;
     res.end('Not Found');
